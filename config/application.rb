89a10b18b89793e4e11d3e0bd8b9e2ebb652cec7
Paulo Henrique Sacramento <contato@henriquesacramento.net>
2017-12-09 23:39:40 -0300

    Add time_zone config.
    Generated by pah version 0.0.28

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,8 @@ module EVotingApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.time_zone = ENV.fetch('TZ', 'Brasilia')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
